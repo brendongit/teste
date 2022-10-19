58d7395cade14ca32442025cceeba1ed35cd4642
brendongit <brenddesigneroficial@gmail.com>
2022-10-19 10:46:54 -0300

    home screen apply buttons

--- src/Screens/Home/index.jsx
+++ src/Screens/Home/index.jsx
@@ -1,22 +1,72 @@
-import React from 'react';
-import { Button, Image, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import circle from '../../../assets/circle.png';
-import logo from '../../../assets/adaptive-icon.png';
+import React from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import circle from "../../../assets/circle.png";
+import logo from "../../../assets/adaptive-icon.png";
 
 const Home = ({ navigation }) => {
-  return ( 
-    <><View style={styles.container}>
-      <Image source={circle} alt="" style={{width: '100%', position: 'absolute'}}/>
-      <Image source={logo} alt="" style={{width: 150, height: 150}}/>
-      <TouchableOpacity>
-        <Text>
-          aaaaa
-        </Text>
-      </TouchableOpacity>
-    </View><Button title="Go to Scan" onPress={() => navigation.navigate('Scan')} /></>
-   );
-}
- 
+  return (
+    <>
+      <View style={styles.container}>
+        <Image
+          source={circle}
+          alt=""
+          style={{ width: "100%", position: "absolute", height: 700 }}
+        />
+        <Image
+          source={logo}
+          alt=""
+          style={{ width: 160, height: 160, marginTop: 50 }}
+        />
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            height: 200,
+            width: "100%",
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Scan")}
+            style={{
+              backgroundColor: "#18822F",
+              width: "50%",
+              height: 40,
+              borderRadius: 20,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 20 }}>
+              Adicionar nota
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#035FB4",
+              width: "50%",
+              height: 40,
+              marginTop: 25,
+              borderRadius: 20,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 20 }}>
+              Gerar nota
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
+  );
+};
+
 export default Home;
 
 const styles = StyleSheet.create({
@@ -24,12 +74,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: '#101114',
+    backgroundColor: "#101114",
   },
   text: {
     fontSize: 22,
-    fontWeight: 'bold',
-    color: '#fff',
-    textAlign: 'center',
-  }
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+  },
 });
